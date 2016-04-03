78ea7533d7ad1fd443ed0c9afb5a56664a88604a
Luke Smith <lukedansmi@sbcglobal.net>
2016-04-03 14:30:10 -0500

    Downloaded and unzipping data

--- Assignments/Exploratory Data Analysis/CourseProject2/plots.R
+++ Assignments/Exploratory Data Analysis/CourseProject2/plots.R
@@ -1,4 +1,7 @@
 # download the data
-url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
-download.file(url, "data.zip")
-unzip("data.zip")
+url    <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+wrkdir <- paste(getwd(), "Assignments/Exploratory Data Analysis/CourseProject2", sep = "/")
+setwd(wrkdir)
+dest   <- paste(getwd(), "data.zip", sep = "/")
+download.file(url, dest)
+unzip(dest)
